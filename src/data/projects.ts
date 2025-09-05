fc10b4dca02adf997c139ebe882fa5d1f0a8c542
Connor <connormorgan@live.ca>
2025-09-05 10:50:06 -0400

    feat: update captions

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -15,22 +15,22 @@ export const PROJECTS: Record<Category, ProjectItem[]> = {
   [Category.GENERAL]: [
     {
       id: "devply",
-      imageSrc: "/icon/devply2.webp",
+      imageSrc: "/icon/devply2.png",
       title: "Devply",
       body: "A two-sided municipal development application tracking system for City Planners and Developers",
       link: "https://devply.com",
     },
     {
       id: "fish",
-      imageSrc: "/icon/coming-soon.webp",
+      imageSrc: "/icon/coming-soon.png",
       title: "Coming Soon",
       body: "Now you're probably thinking to yourself, 'Surely he didn't make a website just to showcase one project right?'. Well you should click on the fish.",
     },
   ],
   [Category.FISHING]: [
-    { id: "bass", imageSrc: "/icon/bass-catch.webp", title: "Good ol' Bass Fishing", body: "Nothing more fun than fighting a bass on some lightweight gear. They'll hit just about anything, even my bright pink lure" },
-    { id: "lake-trout", imageSrc: "/icon/lake-trout-catch.webp", title: "Lake Trout Jigging", body: "This was my first ever lake trout, I was in over 60ft of water and said screw it, let's put something at the bottom, and sure enough, I pulled out this guy!" },
-    { id: "salmon", imageSrc: "/icon/salmon-catch.webp", title: "Salmon Run", body: "My first ever King Salmon, probably my proudest fishing moment. I had spent easily 30 hours fishing over the course of a week without even a bite and on the last day of fishing this guy slammed my glow spoon. I fought this thing on bass gear for well over 15 minutes, by some miracle I was able to get it into my pier net and onto shore." },
-    { id: "walleye", imageSrc: "/icon/walleye-catch.webp", title: "Walleye Through The Ice", body: "I flew out to Winnipeg in a snowstorm for this pic. Nearly put our entire truck through the ice driving back home too. That was a crazy trip. Can't wait to do it again next year!" },
+    { id: "bass", imageSrc: "/icon/bass-catch.webp", title: "Good ol' Bass Fishing", body: "Bass will try and eat just about anything and they're unrelenting fighters once they're hooked. It's an awesome combo." },
+    { id: "lake-trout", imageSrc: "/icon/lake-trout-catch.webp", title: "Lake Trout Jigging", body: "In over 60ft of water we decided to drop our lures to the bottom of the lake and not 2 minutes after getting there, we had this guy on." },
+    { id: "salmon", imageSrc: "/icon/salmon-catch.webp", title: "Salmon Run", body: "My first ever King Salmon, probably my proudest fishing moment. I had spent easily 30 hours fishing over the course of a week without even a bite but on my last day of fishing this guy slammed my glow spoon." },
+    { id: "walleye", imageSrc: "/icon/walleye-catch.webp", title: "Walleye Through The Ice", body: "I flew out to Winnipeg in a snowstorm and drove several kilometres on the ice of Lake Winnipeg for this pic. We even broke through the ice at one point driving back home. It was a crazy trip, can't wait to do it again next year!" },
   ],
 };
