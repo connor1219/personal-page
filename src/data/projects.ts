2e6b134308ece21513fb40bd1541ff11ff9ea4b2
Connor <connormorgan@live.ca>
2025-11-18 15:28:22 -0500

    feat: tweak fishing messaging

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -35,9 +35,9 @@ export const PROJECTS: Record<Category, ProjectItem[]> = {
     },
   ],
   [Category.FISHING]: [
-    { id: "bass", imageSrc: "/icon/bass-catch.webp", title: "Good ol' Bass Fishing", body: "Bass will try and eat just about anything and they're unrelenting fighters once they're hooked. It's an awesome combo." },
+    { id: "bass", imageSrc: "/icon/bass-catch.webp", title: "Good ol' Bass Fishing", body: "Bass will try and eat just about anything and they're surprisingly strong fighters once they're hooked. It's an awesome combo." },
     { id: "lake-trout", imageSrc: "/icon/lake-trout-catch.webp", title: "Lake Trout Jigging", body: "My first ever Lake Trout, caught in over 60ft of water. We dropped our lures to the bottom, and within minutes this guy hit." },
-    { id: "salmon", imageSrc: "/icon/salmon-catch.webp", title: "Salmon Run", body: "After 30 hours with no bites, I finally hooked my first King Salmon on the last day. Totally worth the grind." },
-    { id: "walleye", imageSrc: "/icon/walleye-catch.webp", title: "Walleye Through The Ice", body: "We drove kilometres across frozen Lake Winnipeg in a blizzard for this catch. Wild trip—can’t wait to go again." },
+    { id: "salmon", imageSrc: "/icon/salmon-catch.webp", title: "Salmon Run", body: "After 30 hours of fishing without a single bite, I finally hooked my first King Salmon. Totally worth the grind." },
+    { id: "walleye", imageSrc: "/icon/walleye-catch.webp", title: "Walleye Through The Ice", body: "We drove kilometres across frozen Lake Winnipeg in a blizzard for this catch. Wild trip, can’t wait to go again." },
   ],
 };
